0dea8266ec259b0a17203d2f88065ce3f230cc14
n-scovell <N8scovell@gmail.com>
2025-11-22 19:53:33 -0500

    Added selections
    I thought it would be cool if you could select sections. Which leads me to contemplate a drag and drop situation.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'      
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
